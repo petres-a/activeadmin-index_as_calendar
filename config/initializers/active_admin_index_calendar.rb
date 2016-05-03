aef6c46e81ce29e278de11e198b52c46a7982d37
petres-a <andrei.petrescu@hotmail.fr>
2016-05-03 04:41:07 +0300

    Update active_admin_index_calendar.rb

--- config/initializers/active_admin_index_calendar.rb
+++ config/initializers/active_admin_index_calendar.rb
@@ -1,3 +1,4 @@
+require 'pry'
 module ActiveAdmin
   module Views
     class IndexAsCalendar < ActiveAdmin::Component
@@ -6,7 +7,9 @@ module ActiveAdmin
         add_class "calendar"
         context = {:page_presenter => page_presenter, :collection => collection, :fullCalendarOptions => nil}
         events = instance_exec(context, &page_presenter.block) unless page_presenter.block.blank?
-        designers = page_presenter.designers unless page_presenter.designers.blank?
+        binding.pry
+        designers = nil
+        #designers = page_presenter.designers unless page_presenter.designers.blank?
 
         # Render fullCalendar
         panel "Calendar", id: "calendar" do
